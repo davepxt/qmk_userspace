d818becf2d8c29bd11b01e72a3e5afc665525ce9
dave <davepeix@gmail.com>
2025-12-22 09:40:44 -0300

    chore: update lily58 keymap

--- keyboards/lily58/rev1/keymaps/davepxt/keymap.c
+++ keyboards/lily58/rev1/keymaps/davepxt/keymap.c
@@ -2,6 +2,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
+//    ┌───┬───────┬───────┬───────┬───────┬──────────┐           ┌──────────┬──────────┬───────┬───────┬───────┬───┐
+//    │   │       │       │       │       │          │           │          │          │       │       │       │   │
+//    ├───┼───────┼───────┼───────┼───────┼──────────┤           ├──────────┼──────────┼───────┼───────┼───────┼───┤
+//    │   │   q   │   w   │   e   │   r   │    t     │           │    y     │    u     │   i   │   o   │   p   │   │
+//    ├───┼───────┼───────┼───────┼───────┼──────────┤           ├──────────┼──────────┼───────┼───────┼───────┼───┤
+//    │   │ ◆ (a) │ ⎇ (s) │ ⇧ (d) │ ⎈ (f) │    g     │           │    h     │  ⎈ (j)   │ ⇧ (k) │ ⎇ (l) │ ◆ (') │   │
+//    ├───┼───────┼───────┼───────┼───────┼──────────┼───┐   ┌───┼──────────┼──────────┼───────┼───────┼───────┼───┤
+//    │   │   z   │   x   │   c   │   v   │    b     │   │   │   │    n     │    m     │   ,   │   .   │   /   │   │
+//    └───┴───────┴───────┼───────┼───────┼──────────┼───┤   ├───┼──────────┼──────────┼───────┼───────┴───────┴───┘
+//                        │       │   ⎋   │ LT(1, ␣) │ ⇥ │   │ ⏎ │ LT(2, ⌫) │ LT(3, ⌦) │       │
+//                        └───────┴───────┴──────────┴───┘   └───┴──────────┴──────────┴───────┘
   KC_NO , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO         ,                       KC_NO          , KC_NO         , KC_NO        , KC_NO        , KC_NO           , KC_NO,
   KC_NO , KC_Q         , KC_W         , KC_E         , KC_R         , KC_T          ,                       KC_Y           , KC_U          , KC_I         , KC_O         , KC_P            , KC_NO,
   KC_NO , LGUI_T(KC_A) , LALT_T(KC_S) , LSFT_T(KC_D) , LCTL_T(KC_F) , KC_G          ,                       KC_H           , RCTL_T(KC_J)  , RSFT_T(KC_K) , RALT_T(KC_L) , RGUI_T(KC_QUOT) , KC_NO,
@@ -10,14 +21,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [1] = LAYOUT(
-  KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO  ,                      KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO,
-  KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO  ,                      KC_MUTE , KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , KC_NO,
-  KC_NO , KC_RGUI , KC_RALT , KC_RSFT , KC_RCTL , KC_NO  ,                      KC_MPLY , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_NO,
-  KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO  , KC_NO  ,     KC_NO , KC_INS  , KC_END  , KC_PGDN , KC_PGUP , KC_HOME , KC_NO,
-                              KC_NO   , KC_ESC  , KC_SPC , KC_TAB ,     KC_NO , KC_NO   , KC_NO   , KC_NO
+//    ┌───┬───┬───┬───┬───┬───┐           ┌─────┬──────┬──────┬──────┬─────┬───┐
+//    │   │   │   │   │   │   │           │     │      │      │      │     │   │
+//    ├───┼───┼───┼───┼───┼───┤           ├─────┼──────┼──────┼──────┼─────┼───┤
+//    │   │   │   │   │   │   │           │    │     │     │     │    │   │
+//    ├───┼───┼───┼───┼───┼───┤           ├─────┼──────┼──────┼──────┼─────┼───┤
+//    │   │ ◆ │ ⎇ │ ⇧ │ ⎈ │   │           │    │  ←   │  ↓   │  ↑   │  →  │   │
+//    ├───┼───┼───┼───┼───┼───┼───┐   ┌───┼─────┼──────┼──────┼──────┼─────┼───┤
+//    │   │   │   │   │   │   │   │   │   │ Ins │ Home │ Pg ↓ │ Pg ↑ │ End │   │
+//    └───┴───┴───┼───┼───┼───┼───┤   ├───┼─────┼──────┼──────┼──────┴─────┴───┘
+//                │   │ ⎋ │   │ ⇥ │   │ ⏎ │  ⌫  │  ⌦   │      │
+//                └───┴───┴───┴───┘   └───┴─────┴──────┴──────┘
+  KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,                       KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO,
+  KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,                       KC_MUTE , KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , KC_NO,
+  KC_NO , KC_RGUI , KC_RALT , KC_RSFT , KC_RCTL , KC_NO ,                       KC_MPLY , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_NO,
+  KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO  ,     KC_NO  , KC_INS  , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , KC_NO,
+                              KC_NO   , KC_ESC  , KC_NO , KC_TAB ,     KC_ENT , KC_BSPC , KC_DEL  , KC_NO
 ),
 
 [2] = LAYOUT(
+//    ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
+//    │   │   │   │   │   │   │           │   │   │   │   │   │   │
+//    ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+//    │   │ [ │ 7 │ 8 │ 9 │ ] │           │   │   │   │   │   │   │
+//    ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+//    │   │ ; │ 4 │ 5 │ 6 │ = │           │   │ ⎈ │ ⇧ │ ⎇ │ ◆ │   │
+//    ├───┼───┼───┼───┼───┼───┼───┐   ┌───┼───┼───┼───┼───┼───┼───┤
+//    │   │ ` │ 1 │ 2 │ 3 │ \ │   │   │   │   │   │   │   │   │   │
+//    └───┴───┴───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┴───┴───┘
+//                │   │   │ 0 │ - │   │   │   │   │   │
+//                └───┴───┴───┴───┘   └───┴───┴───┴───┘
   KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO   ,                       KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO,
   KC_NO , KC_LBRC , KC_7  , KC_8  , KC_9  , KC_RBRC ,                       KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO,
   KC_NO , KC_SCLN , KC_4  , KC_5  , KC_6  , KC_EQL  ,                       KC_NO , KC_RCTL , KC_RSFT , KC_RALT , KC_RGUI , KC_NO,
@@ -26,6 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [3] = LAYOUT(
+//    ┌───┬─────┬────┬────┬───────┬───┐           ┌───┬───┬───┬───┬───┬───┐
+//    │   │     │    │    │       │   │           │   │   │   │   │   │   │
+//    ├───┼─────┼────┼────┼───────┼───┤           ├───┼───┼───┼───┼───┼───┤
+//    │   │ f12 │ f7 │ f8 │  f9   │ ⎘ │           │   │   │   │   │   │   │
+//    ├───┼─────┼────┼────┼───────┼───┤           ├───┼───┼───┼───┼───┼───┤
+//    │   │ f11 │ f4 │ f5 │  f6   │ ⤓ │           │   │ ⎈ │ ⇧ │ ⎇ │ ◆ │   │
+//    ├───┼─────┼────┼────┼───────┼───┼───┐   ┌───┼───┼───┼───┼───┼───┼───┤
+//    │   │ f10 │ f1 │ f2 │  f3   │ ‖ │   │   │   │   │   │   │   │   │   │
+//    └───┴─────┴────┼────┼───────┼───┼───┤   ├───┼───┼───┼───┼───┴───┴───┘
+//                   │    │ DF(4) │ ␣ │ ⇥ │   │   │   │   │   │
+//                   └────┴───────┴───┴───┘   └───┴───┴───┴───┘
   KC_NO , KC_NO  , KC_NO , KC_NO , KC_NO , KC_NO   ,                      KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO,
   KC_NO , KC_F12 , KC_F7 , KC_F8 , KC_F9 , KC_PSCR ,                      KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO,
   KC_NO , KC_F11 , KC_F4 , KC_F5 , KC_F6 , KC_SCRL ,                      KC_NO , KC_RCTL , KC_RSFT , KC_RALT , KC_RGUI , KC_NO,
@@ -34,10 +78,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [4] = LAYOUT(
-  KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO  ,                       KC_NO   , KC_NO , KC_NO   , KC_NO  , KC_NO   , KC_NO  ,
-  KC_LSFT , KC_Q  , KC_W  , KC_E  , KC_R    , KC_T   ,                       KC_Y    , KC_U  , KC_I    , KC_O   , KC_P    , KC_RSFT,
-  KC_LCTL , KC_A  , KC_S  , KC_D  , KC_F    , KC_G   ,                       KC_H    , KC_J  , KC_K    , KC_L   , KC_QUOT , KC_RCTL,
-  KC_LALT , KC_Z  , KC_X  , KC_C  , KC_V    , KC_B   , KC_NO  ,     KC_NO  , KC_N    , KC_M  , KC_COMM , KC_DOT , KC_SLSH , KC_RALT,
-                            KC_NO , KC_TRNS , KC_SPC , KC_TAB ,     KC_ENT , KC_BSPC , DF(0) , KC_NO
+//    ┌───┬───┬───┬───┬───┬───┐           ┌───┬───────┬───┬───┬───┬───┐
+//    │   │   │   │   │   │   │           │   │       │   │   │   │   │
+//    ├───┼───┼───┼───┼───┼───┤           ├───┼───────┼───┼───┼───┼───┤
+//    │ ⇧ │ q │ w │ e │ r │ t │           │ y │   u   │ i │ o │ p │ ⇧ │
+//    ├───┼───┼───┼───┼───┼───┤           ├───┼───────┼───┼───┼───┼───┤
+//    │ ⎈ │ a │ s │ d │ f │ g │           │ h │   j   │ k │ l │ ' │ ⎈ │
+//    ├───┼───┼───┼───┼───┼───┼───┐   ┌───┼───┼───────┼───┼───┼───┼───┤
+//    │ ⎇ │ z │ x │ c │ v │ b │   │   │   │ n │   m   │ , │ . │ / │ ⎇ │
+//    └───┴───┴───┼───┼───┼───┼───┤   ├───┼───┼───────┼───┼───┴───┴───┘
+//                │   │ ⎋ │ ␣ │ ⇥ │   │ ⏎ │ ⌫ │ DF(0) │   │
+//                └───┴───┴───┴───┘   └───┴───┴───────┴───┘
+  KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO  , KC_NO  ,                       KC_NO   , KC_NO , KC_NO   , KC_NO  , KC_NO   , KC_NO  ,
+  KC_LSFT , KC_Q  , KC_W  , KC_E  , KC_R   , KC_T   ,                       KC_Y    , KC_U  , KC_I    , KC_O   , KC_P    , KC_RSFT,
+  KC_LCTL , KC_A  , KC_S  , KC_D  , KC_F   , KC_G   ,                       KC_H    , KC_J  , KC_K    , KC_L   , KC_QUOT , KC_RCTL,
+  KC_LALT , KC_Z  , KC_X  , KC_C  , KC_V   , KC_B   , KC_NO  ,     KC_NO  , KC_N    , KC_M  , KC_COMM , KC_DOT , KC_SLSH , KC_RALT,
+                            KC_NO , KC_ESC , KC_SPC , KC_TAB ,     KC_ENT , KC_BSPC , DF(0) , KC_NO
 )
 };
---
+++ users/davepxt/layout_br.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+enum br_keycodes {
+  KC_CDILA = ALGR(KC_COMMA),
+};
