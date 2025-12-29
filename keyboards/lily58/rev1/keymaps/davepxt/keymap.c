e6b67d31a148c7cf7543806af641b40a775947bd
David Peixoto <145052402+davepxt@users.noreply.github.com>
2025-12-29 15:32:56 -0300

    use `LALT_T` instead of `RALT_T` on keymap.c

--- keyboards/lily58/rev1/keymaps/davepxt/keymap.c
+++ keyboards/lily58/rev1/keymaps/davepxt/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                        └───────┴───────┴──────────┴───┘   └───┴──────────┴──────────┴───────┘
   KC_NO , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO         ,                       KC_NO          , KC_NO         , KC_NO        , KC_NO        , KC_NO           , KC_NO,
   KC_NO , KC_Q         , KC_W         , KC_E         , KC_R         , KC_T          ,                       KC_Y           , KC_U          , KC_I         , KC_O         , KC_P            , KC_NO,
-  KC_NO , LGUI_T(KC_A) , LALT_T(KC_S) , LSFT_T(KC_D) , LCTL_T(KC_F) , KC_G          ,                       KC_H           , RCTL_T(KC_J)  , RSFT_T(KC_K) , RALT_T(KC_L) , RGUI_T(KC_QUOT) , KC_NO,
+  KC_NO , LGUI_T(KC_A) , LALT_T(KC_S) , LSFT_T(KC_D) , LCTL_T(KC_F) , KC_G          ,                       KC_H           , RCTL_T(KC_J)  , RSFT_T(KC_K) , LALT_T(KC_L) , RGUI_T(KC_QUOT) , KC_NO,
   KC_NO , KC_Z         , KC_X         , KC_C         , KC_V         , KC_B          , KC_NO  ,     KC_NO  , KC_N           , KC_M          , KC_COMM      , KC_DOT       , KC_SLSH         , KC_NO,
                                         KC_NO        , KC_ESC       , LT(1, KC_SPC) , KC_TAB ,     KC_ENT , LT(2, KC_BSPC) , LT(3, KC_DEL) , KC_NO
 ),
